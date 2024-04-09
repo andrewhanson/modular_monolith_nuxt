3aaa42dff0c00a49d199d77bebabe20da1d85faa
Andrew Hanson <andrewhanson@users.noreply.github.com>
2024-04-09 13:02:18 -0600

    checkpoint

--- apps/web/components/layouts/drawer-links.vue
+++ apps/web/components/layouts/drawer-links.vue
@@ -1,9 +1,11 @@
 <template>
+ 
   <v-list-item density="compact" link title="Home" to="/">
     <template v-slot:prepend>
       <v-icon icon="mdi-home-outline"></v-icon>
     </template>
   </v-list-item>
+  
   <v-list-item density="compact" link title="Recipes" to="recipes">
     <template v-slot:prepend>
       <v-icon icon="mdi-bowl-mix-outline"></v-icon>
--- libs/auth/components/RequiredWrapper.vue
+++ libs/auth/components/RequiredWrapper.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-import useAuth0 from '@auth0/auth0-vue'
+import { useAuth0 } from '@auth0/auth0-vue'
 
 const { isAuthenticated } = useAuth0()
 </script>
